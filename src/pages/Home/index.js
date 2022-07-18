729cd8d7f1965c3caf8ecd8e9645165f8c045178
Gabriel Freire <gabrielfreirebraz@gmail.com>
2022-07-18 00:36:54 -0300

    fix: reset project

--- src/pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles';
-
-export default function Home() {
-  return (
-    <Container>
-      <h1>Minha home</h1>
-    </Container>
-  );
-}
--- src/pages/Home/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  background: black;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
